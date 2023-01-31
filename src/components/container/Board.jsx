3958aa60bdde75f499b68a469f90191d29439c30
GabrielDgt <gaboz.03@gmail.com>
2023-01-31 20:18:38 +0100

    added saving game funcionality via  localStorage

--- src/components/container/Board.jsx
+++ src/components/container/Board.jsx
@@ -7,32 +7,47 @@ import PlayerTurn from '../pure/PlayerTurn.jsx';
 import confetti from "canvas-confetti";
 
 const BoardContainer = () => {
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [playerTurn, setPlayerTurn] = useState(TURN.X);
+  const [board, setBoard] = useState(() => {
+    const storedBoard = window.localStorage.getItem('board')
+    if(storedBoard) return JSON.parse(storedBoard)
+      return Array(9).fill(null)
+  });
+  const [playerTurn, setPlayerTurn] = useState(() => {
+    const storedTurn = window.localStorage.getItem('turn')
+      return storedTurn ?? TURN.X
+  });
+  //setting draw if there is no winner
   const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
     setBoard(Array(9).fill(null));
     setPlayerTurn(TURN.X);
     setWinner(null);
+    //reset local storage
+    window.localStorage.removeItem('board')
+    window.localStorage.removeItem('turn')
   };
 
   const updateBoard = (index) => {
+    //don't update if not null
     if (board[index] || winner) return;
-
+    //reset board
     const newBoard = [...board];
     newBoard[index] = playerTurn;
     setBoard(newBoard);
-
+    //switch turn
     const nextTurn = playerTurn === TURN.X ? TURN.O : TURN.X;
     setPlayerTurn(nextTurn);
-
+    //saving current movements
+    window.localStorage.setItem('board', JSON.stringify(newBoard))
+    window.localStorage.setItem('turn', nextTurn)
+    //check if theres is a winner
     const newWinner = checkWinner(newBoard);
     if (newWinner) {
       confetti()
       setWinner(newWinner);
     }else if (checkEndGame(newBoard)){
-      setWinner(false)
+      setWinner(false) //draw
     }
   };
 
